a8b6a04ab61bb999f99ee7866248254c62c4f159
Lucrecia <mencialucre@gmail.com>
2023-11-01 17:04:24 -0300

    Cambios en home

--- src/paginas/Home.jsx
+++ src/paginas/Home.jsx
@@ -1,26 +1,35 @@
 import { useEffect, useState } from "react"
 import { obtener20Pokemon } from "../api"
+import { ButtonSiguiente } from "../componentes";
 
 function Home() {
 
     const pokemon_path = 'https://pokeapi.co/api/v2/pokemon/'
 
-    useEffect(() => {
+    const [pokemones, setPokemones] = useState([]);
+    const [url, setUrl] = useState([]);
 
-        obtener20Pokemon(pokemon_path)
-            .then((res) => res.json())
-            .then(
-                
-            )
 
+    useEffect(async () => {
+        const paginasPokemon = await obtener20Pokemon(pokemon_path).then((res) => res.json())
+        setPokemones(paginasPokemon.results)
+        setUrl(paginasPokemon.next)
+        console.log(paginasPokemon.next)
     }, [])
 
     return (
         <>
-            <h1>POKÈDEX</h1>
-            <span>
-
-            </span>
+            <div>
+                <h1>POKÈDEX</h1>
+                {
+                    pokemones.map(pokemon => <p>
+                        {pokemon.name}
+                    </p>)
+                }
+                <ButtonSiguiente 
+                urlSiguientePagina={url}
+                />
+            </div>
         </>
     )
 }
